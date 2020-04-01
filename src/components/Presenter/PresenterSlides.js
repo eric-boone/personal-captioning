d079d6ea3b049014b87e41282bb88d3c6cf69d9b
Eric Boone <therealeric@gmail.com>
2020-04-01 11:03:31 -0600

    slides showing up on presenter page

--- src/components/Presenter/PresenterSlides.js
+++ src/components/Presenter/PresenterSlides.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, Fragment } from "react";
 import { FirebaseContext } from "../../firebase/firebase";
 
 function PresenterSlides(props) {
@@ -42,13 +42,66 @@ function PresenterSlides(props) {
     }
   };
 
+  const theShow = () => {
+    let theShowContainer = [];
+    if (theWholeShebang) {
+      for (let x = 0; x < theWholeShebang.showSlides.length; x++) {
+        const element = theWholeShebang.showSlides[x];
+        console.log("element.slide " + x, element.slide);
+        theShowContainer.push(
+          <Fragment key={x}>
+            <div className="container">
+              <div className="row">
+                <div className="col-m-4">
+                  <p className="text-white-50">
+                    Slide#{element.slide.slideNumber + 1}
+                  </p>
+                  <p>
+                    <small className="text-white-50">Line 1</small>
+                    <br />
+                    {theCharacterName(element.slide.line1.characterId)}:{" "}
+                    {element.slide.line1.line}
+                  </p>
+                  <p>
+                    <small className="text-white-50">Line 2</small>
+                    <br />
+                    {theCharacterName(element.slide.line2.characterId)}:{" "}
+                    {element.slide.line2.line}
+                  </p>
+                  <p>
+                    <small className="text-white-50">Line 3</small>
+                    <br />
+                    {theCharacterName(element.slide.line3.characterId)}:{" "}
+                    {element.slide.line3.line}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Fragment>
+        );
+      }
+      console.log("Print.js - theShow");
+    }
+    return theShowContainer;
+  };
+
+  const theCharacterName = x => {
+    for (let y = 0; y < characters.length; y++) {
+      if (characters[y].id === x) {
+        return characters[y].value;
+      }
+    }
+  };
+
   return (
     <div>
       <p>{JSON.stringify(props)}</p>
-      {showID ? 
-      <p>{showTitle()}</p>
-      // <p>{theWholeShebang}</p>
-      : null}
+      {showID ? (
+        <>
+          <p>{showTitle()}</p>
+          <div>{theShow()}</div>
+        </>
+      ) : null}
     </div>
   );
 }
